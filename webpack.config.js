d51cec1a7a18e3787c9f95e3396d4c2e841fc816
20021179 <fabricflannigan@gmail.com>
2021-11-16 22:35:03 +1300

    build process config ready

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,33 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const fse = require('fs-extra')
+const fse = require('fs-extra');
 
 const postCSSPlugins = [
   require('postcss-import'),
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
-  // If in the future the creator of the postcss-hexrgba package
-  // releases an update (it is version 2.0.1 as I'm writing this)
-  // then it will likely work with PostCSS V8 so you can uncomment
-  // the line below and also install the package with npm.
-  //require('postcss-hexrgba'),
+  require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
-class RunAfterCompile {
-  apply(compiler) {
-    compiler.hooks.done.tap('Copy images', function() {
-      fse.copySync('./app/assets/images', './dist/assets/images')
+class RunAfterCompile{
+  apply(compiler){
+    compiler.hooks.done.tap('Copy images', function(){
+      fse.copySync('./app/assets/images', './docs/assets/images')
     })
   }
 }
 
 let cssConfig = {
   test: /\.css$/i,
-  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
+  use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
 }
 
-let pages = fse.readdirSync('./app').filter(function(file) {
+let pages = fse.readdirSync('./app').filter(function(file){
   return file.endsWith('.html')
-}).map(function(page) {
+}).map(function(page){
   return new HtmlWebpackPlugin({
     filename: page,
     template: `./app/${page}`
@@ -76,7 +72,7 @@ if (currentTask == 'build') {
     use: {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env']
+        presets: ['@babe/preset-env']
       }
     }
   })
@@ -85,7 +81,7 @@ if (currentTask == 'build') {
   config.output = {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   }
   config.mode = 'production'
   config.optimization = {
@@ -94,10 +90,10 @@ if (currentTask == 'build') {
     minimizer: [`...`, new CssMinimizerPlugin()]
   }
   config.plugins.push(
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(), 
     new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
-    new RunAfterCompile()  
-  )
+    new RunAfterCompile()
+    )
 }
 
 module.exports = config
